3e659f054caf28a4b1b6ceea9179dad6f6b8b449
Harvey Mortimore <77932367+hmortimore69@users.noreply.github.com>
2025-02-04 16:55:32 +0000

    Added comments

--- index.js
+++ index.js
@@ -7,6 +7,10 @@
 
 'use strict';
 
+/*
+Create a function `showMessage` that takes two parameters: an element and a string that is a URL. 
+The function will fetch the URL and put the response text into the text content of the provided element. 
+*/
 async function showMessage(elem, url) {
     try {
         const response = await fetch(url);
@@ -22,6 +26,13 @@ async function showMessage(elem, url) {
     }
 }
 
+/*
+Create a function `showList` that takes two parameters: an element and a string that is a URL. 
+The function will fetch the URL, parse the retrieved data as JSON; 
+the data is guaranteed to be an array of strings. 
+The function will then, like the `filler` function in `dom101`, 
+put the contents of the array as list items into the provided element.
+*/
 async function showList(elem, url) {
     try {
         const response = await fetch(url);
@@ -43,6 +54,12 @@ async function showList(elem, url) {
     }
 }
 
+/*
+Create a function `startShowingMessage` that takes two parameters: 
+an element and a string that is a URL. The function will use `setInterval` to make 
+the following task every 1s: fetch the URL and put the response text into the 
+text content of the provided element.
+*/
 function startShowingMessage(elem, url) {
     setInterval(async () => {
         try {
@@ -59,6 +76,11 @@ function startShowingMessage(elem, url) {
     }, 1000);
 }
 
+/*
+Create a function 'handleError' that accepts an element and a url as a parameter, 
+and shows the fetched text from the server in the element. 
+If there is an error, the function should set the textContent of the element to 'OH DEAR'.
+*/
 async function handleError(elem, url) {
     try {
         const response = await fetch(url);
@@ -73,6 +95,13 @@ async function handleError(elem, url) {
     }
 }
 
+/*
+Create a function `drawBox', which accepts two parameters: 
+a canvas element, and a URL which refers to a simple object with coordinates 
+that you should fetch from a server. The function draws a 10x10 filled 
+black box at the given coordinates. Your drawBox function should update the 
+coordinates and redraw the box every 1 second.
+*/
 function drawBox(canvas, url) {
     const ctx = canvas.getContext("2d");
 
