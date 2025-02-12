b593d31d0a05a7ce2d2daa43f89d7e2e5082a9c4
Harvey Mortimore <77932367+hmortimore69@users.noreply.github.com>
2025-02-12 11:13:32 +0000

    Reformatted commenting

--- index.js
+++ index.js
@@ -8,8 +8,10 @@
 'use strict';
 
 /*
-Create a function `showMessage` that takes two parameters: an element and a string that is a URL. 
-The function will fetch the URL and put the response text into the text content of the provided element. 
+ * Input: elem: Element, url: String
+ * Return: None
+ * Create a function `showMessage` that takes two parameters: an element and a string that is a URL. 
+ * The function will fetch the URL and put the response text into the text content of the provided element. 
 */
 async function showMessage(elem, url) {
     try {
@@ -27,12 +29,14 @@ async function showMessage(elem, url) {
 }
 
 /*
-Create a function `showList` that takes two parameters: an element and a string that is a URL. 
-The function will fetch the URL, parse the retrieved data as JSON; 
-the data is guaranteed to be an array of strings. 
-The function will then, like the `filler` function in `dom101`, 
-put the contents of the array as list items into the provided element.
-*/
+ * Input: elem: Element, url: String
+ * Return: None
+ * Create a function `showList` that takes two parameters: an element and a string that is a URL. 
+ * The function will fetch the URL, parse the retrieved data as JSON; 
+ * the data is guaranteed to be an array of strings. 
+ * The function will then, like the `filler` function in `dom101`, 
+ * put the contents of the array as list items into the provided element.
+ */
 async function showList(elem, url) {
     try {
         const response = await fetch(url);
@@ -55,11 +59,13 @@ async function showList(elem, url) {
 }
 
 /*
-Create a function `startShowingMessage` that takes two parameters: 
-an element and a string that is a URL. The function will use `setInterval` to make 
-the following task every 1s: fetch the URL and put the response text into the 
-text content of the provided element.
-*/
+ * Input: elem: Element, url: String
+ * Return: None
+ * Create a function `startShowingMessage` that takes two parameters: 
+ * an element and a string that is a URL. The function will use `setInterval` to make 
+ * the following task every 1s: fetch the URL and put the response text into the 
+ * text content of the provided element.
+ */
 function startShowingMessage(elem, url) {
     setInterval(async () => {
         try {
@@ -77,10 +83,12 @@ function startShowingMessage(elem, url) {
 }
 
 /*
-Create a function 'handleError' that accepts an element and a url as a parameter, 
-and shows the fetched text from the server in the element. 
-If there is an error, the function should set the textContent of the element to 'OH DEAR'.
-*/
+ * Input: elem: Element, url: String
+ * Return: None
+ * Create a function 'handleError' that accepts an element and a url as a parameter, 
+ * and shows the fetched text from the server in the element. 
+ * If there is an error, the function should set the textContent of the element to 'OH DEAR'.
+ */
 async function handleError(elem, url) {
     try {
         const response = await fetch(url);
@@ -96,12 +104,14 @@ async function handleError(elem, url) {
 }
 
 /*
-Create a function `drawBox', which accepts two parameters: 
-a canvas element, and a URL which refers to a simple object with coordinates 
-that you should fetch from a server. The function draws a 10x10 filled 
-black box at the given coordinates. Your drawBox function should update the 
-coordinates and redraw the box every 1 second.
-*/
+ * Input: canvas: Element, url: String
+ * Return: None
+ * Create a function `drawBox', which accepts two parameters: 
+ * a canvas element, and a URL which refers to a simple object with coordinates 
+ * that you should fetch from a server. The function draws a 10x10 filled 
+ * black box at the given coordinates. Your drawBox function should update the 
+ * coordinates and redraw the box every 1 second.
+ */
 function drawBox(canvas, url) {
     const ctx = canvas.getContext("2d");
 
@@ -124,5 +134,3 @@ function drawBox(canvas, url) {
         }
     }, 1000);
 }
-
-
